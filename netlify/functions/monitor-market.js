3f739fbe9c8cb2078ab50803c03bfad2d3105b39
o90746002694243-cpu <o90746002694243@gmail.com>
2026-09-14 08:23:21 +0900

    Update monitor-market.js

--- netlify/functions/monitor-market.js
+++ netlify/functions/monitor-market.js
@@ -4,7 +4,7 @@ exports.handler = async function () {
       "USD/JPY",
     ];
     const interval = "15min";
-    const targetScore = 0;
+    const targetScore = 80;
     const results = [];
 
     for (const pair of pairs) {  
